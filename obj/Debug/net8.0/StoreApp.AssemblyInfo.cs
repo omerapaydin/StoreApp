5f8f34bc384784b1b93af1b0e0e0965f6b76398d
Ömer Apaydın <omerapaydin90@gmail.com>
2025-02-15 22:03:50 +0300

    Düzeltme

--- obj/Debug/net8.0/StoreApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/StoreApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+744a25d95c7d315b400fd23648734a2b7a16b317")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9ec320dcdff671b47b223b99315d8d17ed79be8")]
 [assembly: System.Reflection.AssemblyProductAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
